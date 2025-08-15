483205f1fe2d0d7022ccb708e0235f8f9fe93394
Jagadish Ramakurthi <jagadishramakurthi17@gmail.com>
2025-08-15 15:20:04 -0400

    ingest

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import cnnClassifier # ensures setup runs
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome logger")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
